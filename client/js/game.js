9290e4862f2eb15c27acc19f1a73812dd0d8f185
borbit <serge.borbit@gmail.com>
2011-02-05 14:03:40 +0200

    Small refactoring in js/game.js

--- client/js/game.js
+++ client/js/game.js
@@ -1,7 +1,7 @@
 $(function() {
     var transport = Pong.EventsRemote.Adapters.WS(),
-        publisher = Pong.EventsRemote.Publisher(transport),
-        receiver = Pong.EventsRemote.Receiver(transport),
+        remotePublisher = Pong.EventsRemote.Publisher(transport),
+        remoteReceiver = Pong.EventsRemote.Receiver(transport),
 
         joinButtonLeft = $('#menu .button.left'),
         joinButtonRight = $('#menu .button.right'),
@@ -11,17 +11,17 @@ $(function() {
         menu = $('#menu');
 
     joinButtonLeft.click(function() {
-        publisher.joinLeft();
-        chosenSide = 'left';
+        remotePublisher.joinLeft();
+        assignShield('left');
     });
 
     joinButtonRight.click(function() {
-        publisher.joinRight();
-        chosenSide = 'right';
+        remotePublisher.joinRight();
+        assignShield('right');
     });
     
     transport.subscribe(Pong.WSAdapter.events.CONNECTED, function() {
-        publisher.joinGame('only');
+        remotePublisher.joinGame('only');
         statusMessage.hide();
         joinButtonLeft.show();
         joinButtonRight.show();
@@ -36,7 +36,7 @@ $(function() {
         statusMessage.text('DISCONNECTED').show();
     });
 
-    receiver.subscribe(receiver.events.GAMESTATE, function(packetData) {
+    remoteReceiver.subscribe(remoteReceiver.events.GAMESTATE, function(packetData) {
         if (packetData.gameState == Components.Constants.GAME_STATE_WAITING_FOR_PLAYERS) {
             if (packetData.leftPlayerState == Components.Constants.PLAYER_STATE_CONNECTED) {
                 joinButtonLeft.hide();
@@ -48,35 +48,35 @@ $(function() {
             }
         } else if(packetData.gameState == Components.Constants.GAME_STATE_IN_PROGRESS) {
             menu.hide();
-            createGame();
+            stage.start();
         }
     });
 
-    receiver.subscribe(receiver.events.GAMESNAPSHOT, function(data) {
-        /*shields.left.region.x = data['left'].x;
-        shields.left.region.y = data['left'].y;
-        shields.right.region.x = data['right'].x;
-        shields.right.region.y = data['right'].y;*/
-        ball.region.x = data['ball'].x;
-        ball.region.y = data['ball'].y;
-        ball.kx = data['ball'].kx;
-        ball.ky = data['ball'].ky;
-        ball.angle = data['ball'].angle;
+    remoteReceiver.subscribe(remoteReceiver.events.GAMESNAPSHOT, function(data) {
+        shields.left.region.x = data[shields.left.id].x;
+        shields.left.region.y = data[shields.left.id].y;
+        shields.right.region.x = data[shields.right.id].x;
+        shields.right.region.y = data[shields.right.id].y;
+        ball.region.x = data[ball.id].x;
+        ball.region.y = data[ball.id].y;
+        ball.kx = data[ball.id].kx;
+        ball.ky = data[ball.id].ky;
+        ball.angle = data[ball.id].angle;
     });
 
-    receiver.subscribe(receiver.events.MOVEUP, function(data) {
+    remoteReceiver.subscribe(remoteReceiver.events.MOVEUP, function(data) {
         shields[data.side].region.x = data.x;
         shields[data.side].region.y = data.y;
         shields[data.side].energy = data.energy;
         shields[data.side].moveUp();
     });
-    receiver.subscribe(receiver.events.MOVEDOWN, function(data) {
+    remoteReceiver.subscribe(remoteReceiver.events.MOVEDOWN, function(data) {
         shields[data.side].region.x = data.x;
         shields[data.side].region.y = data.y;
         shields[data.side].energy = data.energy;
         shields[data.side].moveDown();
     });
-    receiver.subscribe(receiver.events.STOP, function(data) {
+    remoteReceiver.subscribe(remoteReceiver.events.STOP, function(data) {
         shields[data.side].region.x = data.x;
         shields[data.side].region.y = data.y;
         shields[data.side].stop();
@@ -85,37 +85,38 @@ $(function() {
     statusMessage.text('CONNECTING').show();
     transport.connect();
 
-    var shields = {left: null, right: null} , ball, chosenSide;
-    var keyboard = Pong.EventsClient.KeyboardReceiver;
+    function assignShield(side) {
+        var keyboard = Pong.EventsClient.KeyboardReceiver;
 
-    function createGame() {
         keyboard.subscribe(keyboard.events.MOVEUP, function() {
-            publisher.shieldMoveUp(chosenSide);
+            remotePublisher.shieldMoveUp(side);
+            shields[side].moveUp();
         });
         keyboard.subscribe(keyboard.events.MOVEDOWN, function() {
-            publisher.shieldMoveDown(chosenSide);
+            remotePublisher.shieldMoveDown(side);
+            shields[side].moveDown();
         });
         keyboard.subscribe(keyboard.events.STOP, function() {
-            publisher.shieldStop(chosenSide);
+            remotePublisher.shieldStop(side);
+            shields[side].stop();
         });
-
-        ball = new Pong.Ball(100, 100, 'ball');
-
-        var stage = Pong.Stage();
-
-        shields.left = new Pong.Shield(40, 250, 'left');
-        shields.right = new Pong.Shield(750, 250, 'right');
-
-        stage.addDynamicElement(shields.left)
-             .addDynamicElement(shields.right)
-             .addDynamicElement(ball)
-             .start();
-
-        var view = Pong.View(stage);
-        var ballsRenderer = Pong.Renderers.Ball();
-        var shieldsRenderer = Pong.Renderers.Shield();
-        view.addRenderer(shields.left.id, $('#shield1'), shieldsRenderer);
-        view.addRenderer(shields.right.id, $('#shield2'), shieldsRenderer);
-        view.addRenderer(ball.id, $('#ball'), ballsRenderer);
     }
+
+    var shields = {
+        left: new Pong.Shield(40, 250, 'left'),
+        right: new Pong.Shield(750, 250, 'right')
+    };
+    var ball = new Pong.Ball(100, 100, 'ball');
+    var stage = Pong.Stage();
+
+    stage.addDynamicElement(shields.left)
+         .addDynamicElement(shields.right)
+         .addDynamicElement(ball);
+
+    var view = Pong.View(stage);
+    var ballRenderer = Pong.Renderers.Ball();
+    var shieldRenderer = Pong.Renderers.Shield();
+    view.addRenderer(shields.left.id, $('#shield1'), shieldRenderer);
+    view.addRenderer(shields.right.id, $('#shield2'), shieldRenderer);
+    view.addRenderer(ball.id, $('#ball'), ballRenderer);
 });
